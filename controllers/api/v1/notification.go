0c3d163eacad3496d8a1bf914522a64e91fdce6a
KienBui1995 <kien.laohac@gmail.com>
2017-05-04 15:18:27 +0700

    try push noti with static token

--- controllers/api/v1/notification.go
+++ controllers/api/v1/notification.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/maddevsio/fcm"
 )
@@ -13,21 +14,21 @@ func PushNotification(deviceid []string) (bool, error) {
 		"sum": "Happy Day",
 	}
 	c := fcm.NewFCM("AAAAuET9LvY:APA91bEYl-fIkcY0w7b6umgBHD4yrZnG_v9I2iY1K3EnjUfSrYvlFYIG5vrmP8wFCH8ZMZ-Kx6U6u3XIsw-AIGehs-msWXtlzOq8R_50qAiqcsrJv9WQluALvjWPqSIAPrVS2RKZ4H6V")
-
+	token := "d2dvP8sjYUI:APA91bEyxraiHo-UKMeBAx-Pt7Mveih2Ydd1dddRK8lxbw-3_gZ78kz3uJWRdRTVgzlKp5_yumpn7dIjIkVoEBWbBRJZaHDJfYt2ydp0atLgfHcyQkOAuNCdEvK_uMM1bBZA7ayyx6HM"
 	response, err := c.Send(fcm.Message{
 		Data:             data,
-		RegistrationIDs:  deviceid,
+		RegistrationIDs:  []string{token},
 		ContentAvailable: true,
 		Priority:         fcm.PriorityHigh,
 		Notification: fcm.Notification{
 			Title: "Follow",
 			Body:  "World",
-			Sound: "",
+			Sound: "default",
 			Badge: "113",
 		},
 	})
 	if err != nil {
-
+		log.Fatal(err)
 		fmt.Println("Status Code   :", response.StatusCode)
 		fmt.Println("Success       :", response.Success)
 		fmt.Println("Fail          :", response.Fail)
